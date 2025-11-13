8e0113e9824bd19e8e83f1e8dc1eb94eff49e005
Dimitris Symeonides <dimitrisymeonidis@gmail.com>
2025-11-13 11:49:56 +0000

    ακομα δεν βλεπω το logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,17 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import { useTheme } from 'next-themes';
 
 export function Logo({ className }: { className?: string }) {
-  const { resolvedTheme } = useTheme();
-
   return (
     <div className={cn('flex items-center justify-center', className)}>
-      <Image
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src="/logo.png"
         alt="WordMates Logo"
         width={300}
         height={150}
-        priority
-        className={cn(resolvedTheme === 'dark' ? 'invert' : '')}
+        className={cn(className)}
       />
     </div>
   );
